ee21ae4d15dd23e388598ff03d0f873ab9a64092
Khánh Nguyễn Hữu <khanhnh@bil.vn>
2025-02-28 12:25:06 +0700

    --deploy

--- source/apps/frontend/view/webpack.config.js
+++ source/apps/frontend/view/webpack.config.js
@@ -156,7 +156,6 @@ const Config = (env, argv) => {
                             presets: ["@babel/preset-env", "@babel/preset-react"],
                             plugins: [
                                 "@babel/plugin-transform-class-properties",
-                                "@babel/plugin-proposal-object-rest-spread"
                             ]
                         }
                     }
